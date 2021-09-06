c246355fa7700acda9ca0e12b585a5d7a935a212
fabien stouder <fabien.stouder@gmail.com>
2021-09-06 11:24:37 +0200

    Deuxieme Commit

--- projetgittest/src/projetgittest/FirstClass.java
+++ projetgittest/src/projetgittest/FirstClass.java
@@ -3,9 +3,11 @@ package projetgittest;
 public class FirstClass {
 
 	private String name;
+	private String ensuite;
 	
 	public FirstClass(){
 		name="je suis la premier class qui sera commité";
+		ensuite = "je suis modifié";
 	}
 
 	public String getName() {
@@ -14,6 +16,14 @@ public class FirstClass {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getEnsuite() {
+		return ensuite;
+	}
+
+	public void setEnsuite(String ensuite) {
+		this.ensuite = ensuite;
 	}
 	
 	
