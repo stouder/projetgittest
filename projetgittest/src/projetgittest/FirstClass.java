e68ee00f53c49591f7812c6867fc6892b6b9de01
fabien stouder <fabien.stouder@gmail.com>
2021-09-06 11:16:38 +0200

    Premier commit

---
+++ projetgittest/src/projetgittest/FirstClass.java
@@ -0,0 +1,20 @@
+package projetgittest;
+
+public class FirstClass {
+
+	private String name;
+	
+	public FirstClass(){
+		name="je suis la premier class qui sera commité";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
