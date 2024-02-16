6aaca7b3f2d535021f2f72bd25e9f529c3000757
fabien stouder <fabien.stouder@gmail.com>
2024-02-16 09:26:52 +0100

    Commit Initial Server Config

---
+++ Config-server-Eureka/src/test/java/com/jwtapi/server/ConfigServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtapi.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
