95f080a2943d55b248263ba654ad18c277a648fc
fabien stouder <fabien.stouder@gmail.com>
2024-02-16 09:25:37 +0100

    Modification bootstrap, pom.xml et class execution application

--- Jwt-Api/src/main/java/com/jwtapi/api/JwtApiApplication.java
+++ Jwt-Api/src/main/java/com/jwtapi/api/JwtApiApplication.java
@@ -3,6 +3,7 @@ package com.jwtapi.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "com.jwtapi.model")
 @EnableJpaRepositories(basePackages = { "com.jwtapi.repository" })
 @EnableAspectJAutoProxy
+@EnableDiscoveryClient
 public class JwtApiApplication {
 
 	public static void main(String[] args) {
